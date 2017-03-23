14dd67f80945b947ff8e6ff10b8bee3463be833b
SirJls <mail@sirjls.nl>
2017-03-23 19:38:59 +0100

    New scripts

--- offlineimap.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-from subprocess import check_output
-
-def get_pass(account):
-    return check_output("pass " + account, shell=True).splitlines()[0]
-
-# vim:nu:ai:et:sw=4:ts=8:
